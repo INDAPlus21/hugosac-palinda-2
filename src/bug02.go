ae7f1b2ebbe9f888e0d0eb10ae38228750f1c6fe
Hugo Sacilotto <hugge.saci@gmail.com>
2022-03-24 09:18:43 +0100

    Cleanup

--- src/bug02.go
+++ src/bug02.go
@@ -6,27 +6,24 @@ import (
 	"time"
 )
 
-var wg sync.WaitGroup // Global wait group
-
 // This program should go to 11, but it seemingly only prints 1 to 10.
 func main() {
 	ch := make(chan int)
+	wg := new(sync.WaitGroup) // wait group
 
 	wg.Add(1) // Add 1 to the counter
 
-	go Print(ch)
+	go Print(ch, wg)
 	for i := 1; i <= 11; i++ {
 		ch <- i
 	}
 	close(ch)
-
 	wg.Wait() // Wait until counter is 0. Waits until Print has finished executing.
-
 }
 
 // Print prints all numbers sent on the channel.
 // The function returns when the channel is closed.
-func Print(ch <-chan int) {
+func Print(ch <-chan int, wg *sync.WaitGroup) {
 	for n := range ch { // reads from channel until it's closed
 		time.Sleep(10 * time.Millisecond) // simulate processing time
 		fmt.Println(n)
